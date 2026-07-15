ddf2e8dca66a1453013b50387bfe6cd743ea21ba
Muh.rifqi aras.a <rifqiaras121@gmail.com>
2026-07-15 22:01:15 +0800

    Pertemuan 2 - Blade Template

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,3 @@
+<x-app title="Create Student">
+    <h1 class="fw-bold">Create Student Form</h1>
+</x-app>
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,3 @@
+<x-app title="Student">
+    <h1 class="fw-bold">Student Data</h1>
+</x-app>
